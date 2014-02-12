5db1585dbc0c5beaa6f049e2dfddbb4d29cc9fc5
Andre Herculano <andresilveirah@gmail.com>
2014-02-12 12:05:42 -0200

    project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Patcha::Application.config.secret_key_base = '658978edddaf064e53452b051fb01d4c88239d7921560d300c7225e4154367e0fd01bbd8c0537bf8e7ecc40c4ceb905d10757d1610fecbe1fe5226710deafe34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Patcha::Application.config.session_store :cookie_store, key: '_patcha_session'
