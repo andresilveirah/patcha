42b9a666cd68e6d26cd7b356bdd0cf29bcc11725
Andre Herculano <andresilveirah@gmail.com>
2014-03-11 16:52:51 -0300

    add authentication page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :lockable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
